8b7c3e8757f496d26557234c72334fe7f56e8d0b
murad adnan <eng.namesw@gmail.com>
2022-09-23 21:17:50 +0300

    Initial Commit

---
+++ TaskMamoon/TaskMamoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMamoonApp.swift
+//  TaskMamoon
+//
+//  Created by murad adnan on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskMamoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
